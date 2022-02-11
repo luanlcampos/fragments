c9ca52b6404ea95d49263d9b3a533366655a7d77
luanlcampos <luanlcampos15@gmail.com>
2022-02-11 17:33:35 -0500

    add endpoint to return a list of frag

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,10 +1,15 @@
 // src/routes/api/get.js
-const { createSuccessResponse } = require('../../response');
+const { Fragment } = require('../../model/fragment');
+const { createErrorResponse, createFragmentsResponse } = require('../../response');
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // TODO: this is just a placeholder to get something working...
-  const successResponse = createSuccessResponse({ fragments: [] });
-  res.status(200).json(successResponse);
+  try {
+    const fragmentList = await Fragment.byUser(req.user);
+    return res.status(200).json(createFragmentsResponse(fragmentList));
+  } catch (err) {
+    return res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
+  }
 };
